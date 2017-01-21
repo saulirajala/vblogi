5eb8598984ab60b7e357f5171e806cbbf89a7926
Sauli Rajala <sauli.rajala@valu.fi>
2017-01-21 20:25:15 +0200

    Add page-header image size

--- src/setup.php
+++ src/setup.php
@@ -49,6 +49,7 @@ add_action('after_setup_theme', function () {
      * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
      */
     add_theme_support('post-thumbnails');
+	add_image_size( 'page-header', 1920, 400 );
 
     /**
      * Enable HTML5 markup support
